2f5a8f59757618c5afcbac1376290b27018d0b41
2021110806 <2021110806@dgu.ac.kr>
2024-10-30 16:28:02 +0900

    feat: 시간표 조회 페이지에 네비게이터 추가, 캘린더 title 변경

--- frontend/blog/src/pages/ViewSchedule.js
+++ frontend/blog/src/pages/ViewSchedule.js
@@ -1,9 +1,12 @@
 import React, { useRef, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import './nav_schedule.css';
+import { useNavigate } from 'react-router-dom'; // useNavigate 임포트
 import eventsData from './data/event.json'; // src 경로에서 파일을 import
 export default function ViewSchedule() {
   const calendarEl = useRef(null);
+  const navigate = useNavigate(); // 여기에 useNavigate 호출
 
   useEffect(() => {
     if (calendarEl.current) {
@@ -27,19 +30,60 @@ export default function ViewSchedule() {
     }
   };
   
-  return (
-    <div style={{ height: '80vh', width: '90vw', margin: '0 auto' }}>
-      <FullCalendar
-        ref={calendarEl}
-        plugins={[timeGridPlugin]}
-        initialView="timeGridWeek"
-        height="80%"
-        headerToolbar={{
-          center: 'title',
-        }}
-        slotDuration="00:30:00"
-        events={fetchEvents} 
-      />
-    </div>
+return (
+    <>
+      {/* 네비게이션 바 */}
+      <nav className="nav_shedule_page">
+        <ul className="nav-links">
+          <li>
+            <button
+              className="main-button"
+              onClick={() => (window.location.href = '/home') }
+              style={{ fontSize: '28px' }}
+            >
+              홈
+            </button>
+          </li>
+          <li>
+            <button className="main-button" onClick={() => navigate('/create')} style={{ fontSize: '28px' }}>
+                근무표 생성
+            </button>
+          </li>
+          <li>
+            <button className="main-button" onClick={() => navigate('/write')} style={{ fontSize: '28px' }}>
+               근무표 작성
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      {/* 메인 컨텐츠 */}
+      <div style={{ height: '80vh', width: '90vw', margin: '0 auto' }}>
+        <div
+          style={{
+            textAlign: 'center',
+            marginBottom: '10px',
+            fontWeight: 'bold',
+            fontSize: '60px',
+          }}
+        >
+          근무표 조회
+        </div>
+
+        <FullCalendar
+          ref={calendarEl}
+          plugins={[timeGridPlugin]}
+          initialView="timeGridWeek"
+          height="80%"
+          headerToolbar={{
+            left: 'title',
+            center: '',
+          }}
+          slotDuration="00:30:00"
+          events={fetchEvents}
+        />
+      </div>
+    </>
   );
 }
+
